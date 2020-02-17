9834078f766568f2fa99c148667e52d7c0d60537
cui <angrynaivecui@gmail.com>
2020-02-17 18:02:28 +0800

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
 
+interface GitFile {
+  name: string;
+  url: string;
+}
+
 function App() {
+  const [code, setCode] = useState('');
+  const [fileList, setFileList] = useState(new Array<GitFile>());
+
+  useEffect(() => {
+    Axios
+    .get('https://api.github.com/repos/angryshhh/my-leetcode/contents')
+    .then(({data}) => {
+      data.pop(); // remove README.md
+      data.sort((a: GitFile, b: GitFile) => parseInt(a.name) - parseInt(b.name));
+      setFileList(data);
+    })
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          fileList.map(gitFile => <li
+            key={parseInt(gitFile.name)}
+          >
+            <button
+              onClick={() => {
+                Axios
+                .get(gitFile.url)
+                .then(({data}) => {
+                  setCode(Buffer.from(data.content, data.encoding).toString());
+                })
+                .catch(err => console.log(err));
+              }}
+            >{gitFile.name}</button>
+          </li>)
+        }
+      </ul>
+      <p>
+        {code}
+      </p>
     </div>
   );
 }
